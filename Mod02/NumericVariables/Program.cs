6a7de6dacbb39ecebd31a9370f765afe8afaebd5
CaptainJackSparrow <SDETAcademy333@gmail.com>
2022-11-16 13:42:54 -0800

    Module 2

---
+++ Mod02/NumericVariables/Program.cs
@@ -0,0 +1,10 @@
+﻿int courseID;
+int lengthInWeeks;
+double myGPA;
+courseID = 101;
+lengthInWeeks = 10;
+myGPA = 0.0;
+
+Console.WriteLine(courseID);
+Console.WriteLine(lengthInWeeks);
+Console.WriteLine(myGPA);
