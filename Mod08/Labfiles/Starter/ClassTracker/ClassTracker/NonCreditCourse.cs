b995a54c3be8dd96a8a5181900114c936b11bc95
SDET-Cadet <SDETAcademy333@gmail.com>
2022-11-21 02:55:23 -0800

    module 8

---
+++ Mod08/Labfiles/Starter/ClassTracker/ClassTracker/NonCreditCourse.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassTracker
+{
+    public class NonCreditCourse : Course
+    {
+        public override float CalculateAverage(float[] arrGrades)
+        {
+            float avg = 0;
+            float sum = 0;
+            int numGrades = arrGrades.Count();
+            foreach (float grade in arrGrades)
+            {
+                sum = sum + grade;
+            }
+            avg = sum / numGrades;
+            return avg;
+        }
+
+        public char CalculateGrade(float grade)
+        {
+            char letterGrade;
+            if (grade >= 90)
+            {
+                letterGrade = 'A';
+            }
+            else if (grade >= 80 && grade <= 89)
+            {
+                letterGrade = 'B';
+            }
+            else if (grade >= 70 && grade <= 79)
+            {
+                letterGrade = 'C';
+            }
+            else if (grade >= 66 && grade <= 69)
+            {
+                letterGrade = 'D';
+            }
+            else
+            {
+                letterGrade = 'F';
+            }
+            return letterGrade;
+        }
+    }
+}
---
+++ Mod08/Labfiles/Starter/ClassTracker/ClassTracker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreditCourse creditCourse = new CreditCourse();
+            creditCourse.CourseTitle = "CS101";
+            creditCourse.CreditHours = 6;
+            creditCourse.Program = "Computer Science";
+            creditCourse.EnrollStudent("Tom Thumb");
+
+            float GPA = creditCourse.CalculateGPA(98.5F);
+            Console.WriteLine("Tom Thumb has a GPA of {0} in course {1}.", GPA, creditCourse.CourseTitle);
+
+            NonCreditCourse nonCreditCourse = new NonCreditCourse();
+            nonCreditCourse.CourseTitle = "Introduction to Excel";
+            char grade = nonCreditCourse.CalculateGrade(87.0F);
+            Console.WriteLine("Tom also received a {0} in {1}", grade, nonCreditCourse.CourseTitle);
+
+            CreditCourse newCreditCourse = new CreditCourse();
+            Console.WriteLine(newCreditCourse.CourseTitle);
+            CreditCourse newCreditCourseTwo = new CreditCourse("CS102", 6, "Computer Science");
+            Console.WriteLine(newCreditCourseTwo.CourseTitle);
+
+
+
+        }
+    }
+}
